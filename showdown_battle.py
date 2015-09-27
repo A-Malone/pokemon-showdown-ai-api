d509ffce1172fe9867bcb0b57549c21c8b20c3dd
A-Malone <a_s_malone@hotmail.com>
2015-09-27 13:24:35 -0400

    finalized username parsing and added team scanning

--- showdown_battle.py
+++ showdown_battle.py
@@ -4,46 +4,48 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-class Move(object):
-    """docstring for Move"""
-    def __init__(self, name):
-        super(Move, self).__init__()
-        self.name = name
+from enum import Enum
 
+#For associating players with their info
+PLAYER_ELEMS = ['leftbar', 'rightbar']
+class Player(Enum):
+    me = 0
+    op = 1
 
 class ShowdownBattle(object):
     """docstring for ShowdownBattle"""
 
-    battle_room_elem = None
+    root = None
 
     my_user = None
     op_user = None
 
     def __init__(self, battle_room_elem):
         super(ShowdownBattle, self).__init__()
-        self.battle_room_elem = battle_room_elem
+        self.root = battle_room_elem
 
-        my_trainer_elem = WebDriverWait(self.battle_room_elem, 2).until(
-            EC.presence_of_element_located((By.XPATH, "//div[@class='leftbar']/div[@class='trainer']"))
-        )
-        self.my_user = my_trainer_elem.find_element_by_tag_name('strong').text
-
-        op_trainer_elem = WebDriverWait(self.battle_room_elem, 2).until(
-            EC.presence_of_element_located((By.XPATH, "//div[@class='rightbar']/div[@class='trainer']"))
-        )
-        self.op_user = my_trainer_elem.find_element_by_tag_name('strong').text
+        self.my_user = self.get_username(Player.me)
+        self.op_user = self.get_username(Player.op)
 
     def __str__(self):
-        print("Battle: {} vs {}".format(my_user, op_user))
+        return "Battle: {} vs {}".format(self.my_user, self.op_user)
+
+    #Static infor methods
+    def get_username(self, player):
+        trainer_elem = WebDriverWait(self.root, 2).until(
+            EC.presence_of_element_located((By.XPATH, "//div[@class='{}']/div[@class='trainer']".format(PLAYER_ELEMS[player.value])))
+        )
+        return trainer_elem.find_element_by_tag_name('strong').text
 
     #Game status methods
-    def get_teams(self):
-        pass
+    def get_team(self, player):
+        team = []
+        xpath = "//div[@class='{}']/div[@class='trainer']/div[@class='teamicons']//span"
+        poke_elems = self.root.find_elements_by_xpath(xpath.format(PLAYER_ELEMS[player.value]))
+        for poke_elem in poke_elems:
+            team.append(poke_elem.get_attribute('title').replace(' (active))',''))
 
-    def get_my_active_pokemon(self):
-        pass
-
-    def get_oponent_active_pokemon(self):
+    def get_active_pokemon(self, player):
         pass
 
     #Actions
